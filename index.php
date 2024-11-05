8ff123a0ccf19310643611685d4cae312eae3345
Rodrigo Silva <148738919+Diggozk@users.noreply.github.com>
2024-11-05 23:38:37 +0000

    .

--- index.php
+++ index.php
@@ -6,10 +6,28 @@
     <title>jogo da velha</title>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+
 </head>
 <body>
-    <div class="flexbox">
+    <div class="container text-center">
+        <div class="row align-items-start">
+          <div class="col">
+            <div class="container text-center">
+                <div class="row align-items-start">
+                  <div class="col">
+                    x
+                  </div>
+                  <div class="col">
+                    x
+                  </div>
+                  <div class="col">
+                    x
+                </div>
+                </div>
+              </div>
+        </div>
+      </div>
 
-    </div>
 </body>
 </html>
