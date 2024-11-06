1944125fcded3d75e79d62982105d3e3ccaec521
Rodrigo Silva <148738919+Diggozk@users.noreply.github.com>
2024-11-06 00:15:39 +0000

    .

--- index.php
+++ index.php
@@ -7,27 +7,20 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container text-center">
-        <div class="row align-items-start">
-          <div class="col">
-            <div class="container text-center">
-                <div class="row align-items-start">
-                  <div class="col">
-                    x
-                  </div>
-                  <div class="col">
-                    x
-                  </div>
-                  <div class="col">
-                    x
-                </div>
-                </div>
-              </div>
-        </div>
-      </div>
-
+  <h1>Jogo da Velha</h1>
+  <div id="tabuleiro">
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+  </div>
 </body>
 </html>
